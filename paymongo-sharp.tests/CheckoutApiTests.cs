9d1a8a41a492c200bb63f93b351b87ff7e1481df
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-11-30 02:55:19 +0800

    :sparkles: Add initial checkout implementation

---
+++ paymongo-sharp.tests/CheckoutApiTests.cs
@@ -0,0 +1,138 @@
+﻿// MIT License
+// 
+// Copyright (c) 2023 Russell Camo (@russkyc)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using paymongo_sharp.Checkouts.Entities;
+
+namespace paymongo_sharp.tests;
+
+public class CheckoutApiTests
+{
+    private readonly IPaymongoClient _client;
+
+    public CheckoutApiTests()
+    {
+        Env.TraversePath().Load();
+        
+        var secretKey = Env.GetString("SECRET_KEY");
+        var publicKey = Env.GetString("PUBLIC_KEY");
+        
+        _client = new PaymongoClient(publicKey: publicKey, secretKey: secretKey);
+    }
+    
+    [Fact]
+    async Task CreateCheckoutSessionWithMinimalDetails()
+    {
+        // Arrange
+        Checkout checkout = new Checkout()
+        {
+            Description = "Test Checkout",
+            LineItems = new []
+            {
+                new LineItem
+                {
+                    Name = "item_name",
+                    Quantity = 1,
+                    Currency = Currency.Php,
+                    Amount = 3500
+                }
+            },
+            PaymentMethodTypes = new[]
+            {
+                PaymentMethod.GCash,
+                PaymentMethod.Card,
+                PaymentMethod.Atome
+            }
+        };
+        
+        // Act
+        Checkout checkoutResult = await _client.Checkouts.CreateCheckoutAsync(checkout);
+        
+        // Assert
+        Assert.NotNull(checkoutResult);
+        Assert.Equivalent(checkout.LineItems,checkoutResult.LineItems, true);
+        Assert.Equal(CheckoutStatus.Active,checkoutResult.Status);
+
+    }
+    
+    [Fact]
+    async Task CreateCheckoutSessionWithFullDetails()
+    {
+        // Arrange
+        Checkout checkout = new Checkout()
+        {
+            Description = "Test Checkout",
+            CancelUrl = "http://127.0.0.1",
+            SuccessUrl = "http://127.0.0.1",
+            LineItems = new []
+            {
+                new LineItem
+                {
+                    Name = "item_name",
+                    Quantity = 1,
+                    Currency = Currency.Php,
+                    Amount = 3500
+                }
+            },
+            PaymentMethodTypes = new[]
+            {
+                PaymentMethod.GCash,
+                PaymentMethod.Card,
+                PaymentMethod.Atome
+            },
+            Billing = new Billing
+            {
+                Name = "TestName",
+                Email = "test@paymongo.com",
+                Phone = "9063364572",
+                Address = new Address
+                {
+                    Line1 = "TestAddress1",
+                    Line2 = "TestAddress2",
+                    PostalCode = "4506",
+                    State = "TestState",
+                    City = "TestCity",
+                    Country = "PH"
+                }
+            },
+            Metadata = new CheckoutMetadata
+            {
+                Notes = "TestNotes",
+                CustomerNumber = "9063364572",
+                Remarks = "TestRemarks"
+            },
+            SendEmailReceipt = true,
+            ShowDescription = true,
+            ShowLineItems = false
+        };
+        
+        // Act
+        Checkout checkoutResult = await _client.Checkouts.CreateCheckoutAsync(checkout);
+        
+        // Assert
+        Assert.NotNull(checkoutResult);
+        Assert.Equivalent(checkout.LineItems,checkoutResult.LineItems, true);
+        Assert.Equivalent(checkout.Billing,checkoutResult.Billing, true);
+        Assert.Equivalent(checkout.Metadata,checkoutResult.Metadata, true);
+        Assert.Equal(CheckoutStatus.Active,checkoutResult.Status);
+
+    }
+}
---
+++ paymongo-sharp/Checkouts/CheckoutClient.cs
@@ -0,0 +1,74 @@
+﻿// MIT License
+// 
+// Copyright (c) 2023 Russell Camo (@russkyc)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using paymongo_sharp.Checkouts.Entities;
+using paymongo_sharp.Core.Enums;
+using paymongo_sharp.Helpers;
+using RestSharp;
+
+namespace paymongo_sharp.Checkouts;
+
+public class CheckoutClient
+{
+    private readonly RestClient _client;
+    
+    private readonly string _publicKey;
+    private readonly string _secretKey;
+
+    public CheckoutClient(string baseUrl, string publicKey, string secretKey)
+    {
+        _client = new RestClient(new RestClientOptions(baseUrl));
+        _publicKey = publicKey;
+        _secretKey = secretKey;
+    }
+
+    public async Task<Checkout> CreateCheckoutAsync(Checkout checkout)
+    {
+        
+        var data = new CheckoutRequestData
+        {
+            Data = new CheckoutRequestAttributes
+            {
+                Attributes = checkout
+            }
+        };
+
+        var body = JsonConvert.SerializeObject(data);
+        
+        var request = RequestHelper.Create("/checkout_sessions",_secretKey,_secretKey, body);
+
+        var restResult = await _client.PostAsync(request);
+
+        if (string.IsNullOrWhiteSpace(restResult.Content))
+        {
+            return new Checkout
+            {
+                Status = CheckoutStatus.Expired
+            };
+        }
+
+        var requestData = JsonConvert.DeserializeObject<CheckoutRequestData>(restResult.Content!)!;
+        return requestData.Data.Attributes;
+    }
+}
