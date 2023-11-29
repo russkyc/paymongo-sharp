e4d5b6dad1899ee7d31c3e143d9ca7cb4c10940c
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-11-30 02:54:38 +0800

    :white_check_mark: Add initial checkout tests

---
+++ paymongo-sharp.tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Xunit;
+global using DotNetEnv;
+
+// paymongo-sharp
+global using paymongo_sharp.Core.Entities;
+global using paymongo_sharp.Core.Enums;
+global using paymongo_sharp.Interfaces;
