29a1f6c1486c284fe51800f9361b6a4c5a3a3105
Russell Camo <32549126+russkyc@users.noreply.github.com>
2023-12-02 20:07:15 +0800

    :white_check_mark: Update links api tests, move fluent assertions reference to global usings

--- paymongo-sharp.tests/IntegrationTests/CheckoutApiTests.cs
+++ paymongo-sharp.tests/IntegrationTests/CheckoutApiTests.cs
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using FluentAssertions;
 using Paymongo.Sharp.Checkouts.Entities;
 
 namespace paymongo_sharp.tests.IntegrationTests;
