480ee90bc435ee12adde8b38efd0aa4455b60e79
russkyc <jrcc1023@gmail.com>
2025-09-13 12:39:29 +0800

    :sparkles: Add a standalone blazor wasm example

---
+++ Paymongo.Sharp.BlazorWasm.Sample/Program.cs
@@ -0,0 +1,14 @@
+using LumexUI.Extensions;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Paymongo.Sharp.BlazorWasm.Sample;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddLumexServices();
+
+await builder.Build().RunAsync();
