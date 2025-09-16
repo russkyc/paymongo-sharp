455afdaa6bc7589f00adcfb705cc2444bdad1cbe
russkyc <jrcc1023@gmail.com>
2025-09-16 15:40:07 +0800

    :sparkles: Add checkouts and sources to wasm demo

--- Paymongo.Sharp.BlazorWasm.Sample/Program.cs
+++ Paymongo.Sharp.BlazorWasm.Sample/Program.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using LumexUI.Extensions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -10,5 +11,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddLumexServices();
+
+var culture = new CultureInfo("en-PH");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 await builder.Build().RunAsync();
